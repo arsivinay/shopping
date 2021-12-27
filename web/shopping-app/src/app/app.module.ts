d5da856deaf16219c46953aa99c94f138cf2a8e6
DhanyaKumar <dhanyakumar.gs@heraizen.com>
2021-12-27 17:03:59 +0530

    homepage,signup,login

--- web/shopping-app/src/app/app.module.ts
+++ web/shopping-app/src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannersComponent } from './components/banners/banners.component';
-
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannersComponent
+    BannersComponent,
+    SignUpPageComponent,
+    HomeComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
